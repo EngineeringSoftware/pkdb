a36e39b01599d4d3c21d0f80b509baab64773ff1
Ivan Grigorik <givan502@gmail.com>
2026-08-25 20:20:41 -0500

    refresh scripts

---
+++ sbatch-scripts/TACC/tacc_ewald.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#SBATCH -A <Your project>
+#SBATCH -J pkdb_ewald_evaluation
+#SBATCH -o ewald_job.out
+#SBATCH -p gh
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -t 06:00:00
+
+# ------------------
+# Main code section
+# ------------------
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    printf "Script requires two paths: \n\t(1) apptainer directory (holds conda_git.sif)\n\t(2) pkdb root directory\n"
+    exit 1
+fi
+
+APPTAINER_DIR="$(realpath "$1")"
+PKDB_ROOT="$(realpath "$2")"
+
+module load tacc-apptainer
+pushd "$APPTAINER_DIR"
+apptainer exec --nv --fakeroot conda_git.sif bash -c '
+    set -e
+    export CUDACXX=/opt/conda/bin/nvcc
+    export CXX=/usr/bin/g++
+    export CC=/usr/bin/gcc
+    eval "$(conda shell.bash hook)"
+    conda activate pkdb
+
+    BENCH_ROOT="$(realpath "$1/benchmarks")"
+    cd "$BENCH_ROOT"
+    python run_ewald_debuggers.py --spaces "DebugCuda, DebugOpenMP"
+' _ "$PKDB_ROOT"
+
+popd
+echo "Done."
